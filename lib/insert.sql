3ac3ce7d70afa44ba332d9aab53983f6ae9d566a
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-12-04 10:34:06 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 22, 'F', 'Brown', 'Kenya', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 10, 'F', 'Black', 'Asia', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 2, 'M', 'Brown', 'America', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 15, 'F', 'White', 'China', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 6, 'M', 'White', 'Kenya', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 12, 'F', 'Yellow', 'Asia', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 12, 'F', 'Chocolate', 'Europe', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 9, 'M', 'Black', 'Australia', 1);
